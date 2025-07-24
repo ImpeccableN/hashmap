cd27451d3db112864a8198c5b4a109e1771ed4cf
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-07-24 17:43:44 +0200

    filled hashMapArray to full capacity with null

--- hashmap.js
+++ hashmap.js
@@ -1,9 +1,12 @@
-import { createLinkedList } from "./linkedlist";
+import { createLinkedList } from "./linkedlist.js";
 
 function createHashMap(){
     const loadfactor = 0.75;
     let capacity = 16;
     
     let hashMapArray = [];
-    
+    for(let i = 0; i < capacity; i++){
+        hashMapArray.push(null);
+    };
+
 }
